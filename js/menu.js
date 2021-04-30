97eb8c1557fa20afdbb8eed110057ec826f5e560
abdurrehmaan <abdurrehmaan0@gmail.com>
2021-04-30 05:04:16 +0500

    created nav bar, social icon sections, email section with reponsivness

--- js/menu.js
+++ js/menu.js
@@ -2,9 +2,6 @@ const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 
-
-
-
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
 
