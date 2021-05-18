086fd4c63e01620165040e331bc09f3c95f18b52
abdurrehmaan <abdurrehmaan0@gmail.com>
2021-05-18 23:59:13 +0500

    exerience tab updated

--- js/experiencetabs.js
+++ js/experiencetabs.js
@@ -1,16 +1,61 @@
-const tabs = document.querySelectorAll([data-tab-target])
-const tabContents = document.querySelectorAll([data-tab-content])
+const listOftabs = document.querySelectorAll('.tabs li')
+const webDev=document.querySelector('.web-dev')
+const webWordpress=document.querySelector('.wordpress-dev')
+const webUI=document.querySelector('.uiux-desg')
+const webGraphics=document.querySelector('.graphic-desg')
 
-tabs.forEach(tab => {
-    tab.addEventListener('click',() => {
-        const target = document.querySelector(tab.dataset.tabTarget)
-        tabContents.forEach(tabContents => {
-            tabContents.classList.remove('active')
-        })
-        tabs.forEach(tab => {
-            tab.classList.remove('active')
-        })
-        tab.classList.add('active')
-        target.classList.add('active')
+listOftabs.forEach (list => {
+    list.addEventListener('click', e => {
+       
+        
+       
+        if(e.target.textContent==='Wordpress Developer')
+        {
+            webDev.classList.add('d-none')
+            webWordpress.classList.remove('d-none')
+            webUI.classList.add('d-none')
+            webGraphics.classList.add('d-none')
+
+           
+        }
+        else if(e.target.textContent==='Web Developer')
+        {
+            webDev.classList.remove('d-none')
+            webWordpress.classList.add('d-none')
+            webUI.classList.add('d-none')
+            webGraphics.classList.add('d-none')
+            
+        }
+        else if(e.target.textContent==='UiUX Designer')
+        {
+            webDev.classList.add('d-none')
+            webWordpress.classList.add('d-none')
+            webUI.classList.remove('d-none')
+            webGraphics.classList.add('d-none')
+           
+        }
+        else if(e.target.textContent==='Graphic Desiner')
+        {
+            webDev.classList.add('d-none')
+            webWordpress.classList.add('d-none')
+            webUI.classList.add('d-none')
+            webGraphics.classList.remove('d-none')
+            
+        }
+
+        listOftabs.forEach (l => {
+        const btn=l.querySelector('.btn')
+        btn.classList.remove('active')
+});
+e.target.classList.remove('active');
+
+
+
+
+
+e.target.classList.add('active')
+
+
+
     })
-})
+});
